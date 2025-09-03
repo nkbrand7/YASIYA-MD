8c01de33313fdfb8baa454ee16704c9549d73e46
nkbrand7 <chatgot908@gmail.com>
2025-09-03 22:56:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~4MZ2QJyb#KS_7i96h4v72A-DObTCwJBXn1QtIMp3uWIovH_6Y0IM'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~AVBUyDiZ#pW6iy7tzE3rHZAslsVAMpX7XYLmqhXSEgXht343BR_A'
 };
